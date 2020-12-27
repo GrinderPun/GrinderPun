fc366c4451b331623785d606879d79082cef4321
GrinderPun <72764369+GrinderPun@users.noreply.github.com>
2020-12-28 04:47:22 +0530

    !

--- Webs/Bouncer/bouncer.js
+++ Webs/Bouncer/bouncer.js
@@ -1,18 +1,47 @@
+//https://www.youtube.com/watch?v=ih20l3pJoeU
+
 //Get Canvas
 var canvas = document.getElementById("gameCanvas");
 //Get canvas context
 var gameCanvas = canvas.getContext("2d");
 
-//Position of ball
-let posX = 100, posY = 100;
-let velocityX = 1, velocityY = 1;
+let balls = [];
 
-//Random Position
-posX = Math.random() * 601;
-posY = Math.random() * 401;
-//Random velocity
-velocityX = Math.random() * 7;   //0 to 6
-velocityX = Math.random() * 7;   //0 to 6
+class Ball
+{
+    constructor(x, y, vx, vy, radios)
+    {
+        this.posX = x;
+        this.posY = y;
+        this.velocityX = vx;
+        this.velocityY = vy;
+        this.ballRadios = radios;
+
+        console.log('Ball Created !');
+    }
+    BallUpdate()
+    {
+        
+    }
+    BallDraw()
+    {
+    }
+}
+
+//Create Balls
+for(let i = 0; i < 12; i++)
+{
+    //Random Radios
+    var radios = Math.random() * 31;
+    //Random Positions
+    var x = 50;
+    var y = 50;
+    //Random Velocity
+    var vx = (Math.random * 8);
+    var vy = (Math.random * 8);
+    //Create maa balls lol..
+    balls[i] = new Ball(x, y, vx, vy, 40);
+}
 
 function update(timestamp)
 {
@@ -20,37 +49,26 @@ function update(timestamp)
 
     //Draw Background
     //Fill color
-    gameCanvas.fillStyle = "black";
+    //gameCanvas.fillStyle = "black";
     //Stroke Color
-    gameCanvas.strokeStyle = "rgb(185, 185, 185)";
-    gameCanvas.fillRect(0, 0, canvas.width, canvas.height);
+    //gameCanvas.strokeStyle = "rgb(185, 185, 185)";
+    //gameCanvas.fillRect(0, 0, canvas.width, canvas.height);
     //END
 
-    //Draw circle
-    gameCanvas.fillStyle = "white";
-    gameCanvas.beginPath();
-    gameCanvas.arc(posX, posY, 12, 0, 2 * Math.PI);
-    gameCanvas.stroke();
-    gameCanvas.fill();
-
-    //Physic and Collisions
-    if((posX >= 600 - 12 || posX <= 0 + 12))
+    //Ball Update
+    for(let i = 0; i < balls.length; i++)
     {
-        velocityX = velocityX * -1;
-    }else if(posY >= 400 - 12|| posY <= 0 + 12){
-        velocityY = velocityY * -1;
+        balls[i].BallUpdate();
+        balls[i].BallDraw();
+
+        //Draw circle
+        gameCanvas.strokeStyle = "rgb(185, 185, 185)";
+        gameCanvas.fillStyle = "white";
+        gameCanvas.beginPath();
+        gameCanvas.arc(balls[i].posX, balls[i].posY, 12, 0, 2 * Math.PI);
+        gameCanvas.stroke();
+        gameCanvas.fill();
     }
-
-    if(posX > 600 || posX < 0)
-    {
-        posX = canvas.width / 2;
-    }else if(posY > 400 || posY < 0)
-    {
-        posY = canvas.height / 2;
-    }
-
-    posX += velocityX;
-    posY += velocityY;
 }
 
 window.requestAnimationFrame(update);
